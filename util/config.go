aaa4728886253a0e2fdbdd0609beecc9dedda741
suke404 <mc3chan2003@gmail.com>
2024-09-10 22:10:13 -0700

    Load config from file & environment variables in Go with Viper

---
+++ util/config.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+// Config stores all configureation of the application
+// The values are read by viper from a config file or environemnt variable
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configuration from file or environment variable
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env") // json, xml
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
