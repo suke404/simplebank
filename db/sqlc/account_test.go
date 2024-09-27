593c20c9e4ac5a2a1adb3e90d9a672275e5b13f9
suke404 <mc3chan2003@gmail.com>
2024-09-26 23:21:08 -0700

    Handle DB errors

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,27 +11,14 @@ import (
 )
 
 func TestCreateaccount(t *testing.T) {
-	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
-		Balance:  util.RandomMoney(),
-		Currency: util.RandomCurrency(),
-	}
-
-	account, err := testQueries.CreateAccount(context.Background(), arg)
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
-
-	require.Equal(t, arg.Owner, account.Owner)
-	require.Equal(t, arg.Balance, account.Balance)
-	require.Equal(t, arg.Currency, account.Currency)
-
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
+	createRandomAccount(t)
 }
 
 func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
+
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
